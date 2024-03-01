20f630714c12e9ef4d41940b0092ead4928d361d
ThilagavathiChandrasekar <cthilagavathi2003@gmail.com>
2024-03-01 19:10:10 +0530

    some message

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import '../Styles/Contact.css';
-// import Project from '../Components/Project';
 import Contactimg from '../Assets/Contact.png';
 import { IoIosSend } from "react-icons/io";
 import { FcContacts } from "react-icons/fc";
+import emailjs from 'emailjs-com';
+
 function Contact() {
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_6du33xi', 'template_e08tl2g', e.target, 'RgmrtbAiPTnY0l4XY')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <div id='contact'>
-        {/* <Project /> */}
-        <h4 data-aos="zoom-in"  data-aos-easing="linear" data-aos-duration="1000" className='contact-head'><FcContacts />Contact</h4>
+        <h4 className='contact-head'><FcContacts />Contact</h4>
         <div className='container'>
             <div className='row'>
                 <div className='col-lg-6 col-md-12 col-sm-12'>
-                    <img src={Contactimg} className='contact-img' data-aos="flip-left" />
+                    <img src={Contactimg} className='contact-img' />
                 </div>
 
                 <div className='col-lg-6 col-md-12 col-sm-12'>
                     <div>
-                        <form data-aos="zoom-in-up"  data-aos-easing="linear" data-aos-duration="1000">
-                        <input type='email' placeholder='Enter email address' className='email'  /><br />
-                         <input type='message' placeholder='Enter  message...' className='msg' /><br />
-                            <button className='btn'>Send message<IoIosSend /></button>
+                        <form onSubmit={sendEmail}>
+                            <input type='name' name='user_name' placeholder='Enter User Name' className='email' /><br />
+                            <input type='email' name='user_email' placeholder='Enter email address' className='email' /><br />
+                            <textarea name='message' placeholder='Enter message...' className='msg' /><br />
+                            <button type='submit' className='btn'>Send message<IoIosSend /></button>
                         </form>
                     </div>
                 </div>
@@ -30,4 +41,4 @@ function Contact() {
   )
 }
 
-export default Contact
+export default Contact;
