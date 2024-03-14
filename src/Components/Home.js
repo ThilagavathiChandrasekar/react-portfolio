8f68000bddf089cfd9722ffa35661b64043db7ed
ThilagavathiChandrasekar <cthilagavathi2003@gmail.com>
2024-03-14 16:01:54 +0530

    some msg

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,23 +10,20 @@ const Home = () => {
 
       <div className="col-lg-6 col-md-12 col-sm-12 avatar">
         <img className="avathar-img" 
-          data-aos="zoom-in"
+          data-aos="flip-left"
           data-aos-duration="1000"
            src={profile1} alt="avathar" 
         />
       </div>
 
       <div className="col-lg-6 col-md-12 col-sm-12 content">
-        <h3 data-aos="fade-zoom-in"
-          data-aos-easing="ease-in-back"
-          data-aos-delay="300"
-          data-aos-offset="0" className="home-head">
+        <h3 data-aos="zoom-out-up"  data-aos-duration="1000" className="home-head">
               I'm Thilagavathi Chandrasekar I build a responsive websites.
         </h3>
 
             <p 
               className="home-about-content" 
-              data-aos="zoom-in" data-aos-anchor-placement="top-center"
+              data-aos="zoom-out-down "  data-aos-duration="100"
               >
             "Passionate frontend developer with a flair for crafting engaging user experiences. Proficient in HTML, CSS, Bootstrap and React js. Committed to creating clean, responsive designs."
             </p>
